b2a544664bc4cb858b563ea3006513e51b7e6fef
yukitos <yukitos@gmail.com>
2013-03-13 18:08:37 +0900

    Add TryLivet01

---
+++ TryLivet01/TryLivet01/MainWindowModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Livet;
+
+namespace TryLivet01
+{
+    class MainWindowModel : ViewModel
+    {
+        public MainWindowModel()
+        {
+            Message = "Hello Livet World!";
+        }
+
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                if (EqualityComparer<string>.Default.Equals(_message, value))
+                    return;
+
+                _message = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
