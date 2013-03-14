ff61e4953f8bf94bb966184a83292e19980f9d8f
yukitos <yukitos@gmail.com>
2013-03-14 13:28:37 +0900

    Add TryLivet03

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ TryLivet
 
 * TryLivet01 : LivetのViewModelにあるプロパティの値をViewで表示してみただけです。
 * TryLivet02 : LivetCallMethodActionを使って、ButtonコントロールのClickイベントに直接ViewModelのメソッドをバインドしてます。
+* TryLivet03 : Messageを受け取ってダイアログを出したりウィンドウを閉じたりします。
---
+++ TryLivet03/TryLivet03/MainWindowModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Livet;
+using Livet.Messaging;
+using Livet.Messaging.Windows;
+
+namespace TryLivet03
+{
+    class MainWindowModel : ViewModel
+    {
+        public MainWindowModel()
+        {
+        }
+
+        public void Init()
+        {
+            Messenger.RaiseAsync(new InformationMessage(
+                Properties.Resources.InformationMessage,
+                Properties.Resources.InformationMessageCaption,
+                System.Windows.MessageBoxImage.Information,
+                "Info"));
+        }
+
+        public void RequestClose(ConfirmationMessage message)
+        {
+            if (message.Response == true)
+            {
+                Messenger.Raise(new WindowActionMessage("Close", WindowAction.Close));
+            }
+        }
+    }
+}
