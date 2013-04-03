eb29974037457f5fceed15a921c76a636782a5b8
yukitos <yukitos@gmail.com>
2013-04-03 14:38:48 +0900

    Add TryLivet04

---
+++ TryLivet04/TryLivet04/Models/ItemViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Livet;
+
+namespace TryLivet04.Models
+{
+    public class ItemViewModel : ViewModel
+    {
+        public ItemViewModel(string tabName)
+        {
+            this.Name = tabName;
+        }
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (EqualityComparer<string>.Default.Equals(_name, value))
+                    return;
+                _name = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
---
+++ TryLivet04/TryLivet04/Models/MainWindowModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Livet;
+
+namespace TryLivet04.Models
+{
+    public class MainWindowModel : ViewModel
+    {
+        public MainWindowModel()
+        {
+            Items.Add(new ItemViewModel("Tab 1"));
+            Items.Add(new ItemViewModel("Tab 2"));
+        }
+
+        private ObservableSynchronizedCollection<ItemViewModel> items = new ObservableSynchronizedCollection<ItemViewModel>();
+
+        public ObservableSynchronizedCollection<ItemViewModel> Items
+        {
+            get { return items; }
+        }
+    }
+}
